77b3f712f52c9a9117b69e44dcbe634f9774b344
sycghj <tzcbz@outlook.com>
2023-06-15 20:24:16 +0800

    修正bug，密码修改后没有及时在DataModel中更新，造成在原密码错误的情况下密码被意外修改。取款时增添总金额限制。

--- src/main/java/xyz/syc/atm/atm/ChangePassUIController.java
+++ src/main/java/xyz/syc/atm/atm/ChangePassUIController.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ChangePassUIController implements ControlledStage{
+public class ChangePassUIController implements ControlledStage {
 
     @FXML
     public TextField oldpasswd;
@@ -13,34 +13,36 @@ public class ChangePassUIController implements ControlledStage{
     @FXML
     public TextField confirmpasswd;
     @FXML
-            public Label warning;
+    public Label warning;
     StageController myController;
+
     @Override
     public void setStageController(StageController stageController) {
         myController = stageController;
     }
 
-    public void onClick(){
+    public void onClick() {
         String temp = newpasswd.getText();
-        if(DataModel.getPasswd().equals(oldpasswd.getText())){
+        if (DataModel.getPasswd().equals(oldpasswd.getText())) {
             char t = temp.charAt(0);
             int count = 1;
             for (int i = 1; i < temp.length(); i++) {
-                if (t==temp.charAt(i)){
-                    count+=1;
+                if (t == temp.charAt(i)) {
+                    count += 1;
                 }
             }
-            if(count==temp.length()){
+            if (count == temp.length()) {
                 warning.setText("密码不能为重复数字！");
                 return;
             }
-            if (newpasswd.getText().equals(confirmpasswd.getText())){
+            if (newpasswd.getText().equals(confirmpasswd.getText())) {
                 Dao.updatePasswd(newpasswd.getText());
+                DataModel.setPasswd(newpasswd.getText());
                 warning.setText("密码修改成功！");
-            }else{
+            } else {
                 warning.setText("两次输入的密码不一致！");
             }
-        }else {
+        } else {
             warning.setText("原密码错误！");
         }
     }
--- src/main/java/xyz/syc/atm/atm/WithdrawUIController.java
+++ src/main/java/xyz/syc/atm/atm/WithdrawUIController.java
@@ -4,29 +4,36 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class WithdrawUIController implements ControlledStage{
-    StageController myController;
+public class WithdrawUIController implements ControlledStage {
     @FXML
     public TextField intextfield;
     @FXML
     public Label tips2;
+    public int count = 0;
+    StageController myController;
+
     @Override
     public void setStageController(StageController stageController) {
         myController = stageController;
     }
-    public void onClick(){
-        int inmoney  = Integer.parseInt(intextfield.getText());
-        if(inmoney>DataModel.getMoney()){
+
+    public void onClick() {
+        int inmoney = Integer.parseInt(intextfield.getText());
+        if (inmoney >= 5000 || count >= 5000) {
+            tips2.setText("总取款额度不能大于5000元！");
+            tips2.setStyle("-fx-text-fill: red");
+        }
+        if (inmoney > DataModel.getMoney()) {
             tips2.setText("余额不足，请重新输入！");
             tips2.setStyle("-fx-text-fill: red");
             return;
         }
-        if(inmoney%100==0&&inmoney>0){
+        if (inmoney % 100 == 0 && inmoney > 0) {
             Dao.updateMoney(-inmoney);
-            DataModel.setMoney(DataModel.getMoney()-inmoney);
-            tips2.setText("取款成功，当前余额："+DataModel.getMoney());
+            DataModel.setMoney(DataModel.getMoney() - inmoney);
+            tips2.setText("取款成功，当前余额：" + DataModel.getMoney());
             tips2.setStyle("-fx-text-fill: green");
-        }else{
+        } else {
             tips2.setText("取款金额错误，请重新输入！");
             tips2.setStyle("-fx-text-fill: red");
         }
